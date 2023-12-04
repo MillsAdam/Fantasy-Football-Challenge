a51f994029f13f3c0a1d506b9c8853fddae682a5
Adam Mills <adam.mills.ud@gmail.com>
2023-12-04 16:35:59 -0500

    Stylized front end for stats page

--- CSharp-React/react-app/src/components/StatsComponent.js
+++ CSharp-React/react-app/src/components/StatsComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect, useCallback } from "react";
 import { AuthContext } from "../context/AuthContext";
 import StatsService from "../services/StatsService";
+import DatabaseService from "../services/DatabaseService";
 import "../styles/StatsComponent.css";
 
 const positionOptions = ['qb', 'rb', 'wr', 'te', 'flex', 'k', 'def'];
@@ -59,40 +60,39 @@ const conferenceDisplayOptions = {
     'afc': 'AFC',
     'nfc': 'NFC'
 };
-const teamOptions = ['ari', 'atl', 'bal', 'buf', 'car', 'chi', 'cin', 'cle', 'dal', 'den', 'det', 'gb', 'hou', 'ind', 'jax', 'kc', 'lac', 'lar', 'lv', 'mia', 'min', 'ne', 'no', 'nyg', 'nyj', 'phi', 'pit', 'sea', 'sf', 'tb', 'ten', 'wsh'];
-const teamDisplayOptions = {
-    'ari': 'Arizona Cardinals',
-    'atl': 'Atlanta Falcons',
-    'bal': 'Baltimore Ravens',
-    'buf': 'Buffalo Bills',
-    'car': 'Carolina Panthers',
-    'chi': 'Chicago Bears',
-    'cin': 'Cincinnati Bengals',
-    'cle': 'Cleveland Browns',
-    'dal': 'Dallas Cowboys',
-    'den': 'Denver Broncos',
-    'det': 'Detroit Lions',
-    'gb': 'Green Bay Packers',
-    'hou': 'Houston Texans',
-    'ind': 'Indianapolis Colts',
-    'jax': 'Jacksonville Jaguars',
-    'kc': 'Kansas City Chiefs',
-    'lac': 'Los Angeles Chargers',
-    'lar': 'Los Angeles Rams',
-    'lv': 'Las Vegas Raiders',
-    'mia': 'Miami Dolphins',
-    'min': 'Minnesota Vikings',
-    'ne': 'New England Patriots',
-    'no': 'New Orleans Saints',
-    'nyg': 'New York Giants',
-    'nyj': 'New York Jets',
-    'phi': 'Philadelphia Eagles',
-    'pit': 'Pittsburgh Steelers',
-    'sea': 'Seattle Seahawks',
-    'sf': 'San Francisco 49ers',
-    'tb': 'Tampa Bay Buccaneers',
-    'ten': 'Tennessee Titans',
-    'wsh': 'Washington Football Team'
+const teamNameDisplayOptions = {
+    'ARI': 'Arizona Cardinals',
+    'ATL': 'Atlanta Falcons',
+    'BAL': 'Baltimore Ravens',
+    'BUF': 'Buffalo Bills',
+    'CAR': 'Carolina Panthers',
+    'CHI': 'Chicago Bears',
+    'CIN': 'Cincinnati Bengals',
+    'CLE': 'Cleveland Browns',
+    'DAL': 'Dallas Cowboys',
+    'DEN': 'Denver Broncos',
+    'DET': 'Detroit Lions',
+    'GB': 'Green Bay Packers',
+    'HOU': 'Houston Texans',
+    'IND': 'Indianapolis Colts',
+    'JAX': 'Jacksonville Jaguars',
+    'KC': 'Kansas City Chiefs',
+    'LAC': 'Los Angeles Chargers',
+    'LAR': 'Los Angeles Rams',
+    'LV': 'Las Vegas Raiders',
+    'MIA': 'Miami Dolphins',
+    'MIN': 'Minnesota Vikings',
+    'NE': 'New England Patriots',
+    'NO': 'New Orleans Saints',
+    'NYG': 'New York Giants',
+    'NYJ': 'New York Jets',
+    'PHI': 'Philadelphia Eagles',
+    'PIT': 'Pittsburgh Steelers',
+    'SEA': 'Seattle Seahawks',
+    'SF': 'San Francisco 49ers',
+    'TB': 'Tampa Bay Buccaneers',
+    'TEN': 'Tennessee Titans',
+    'WAS': 'Washington Football Team'
 };
 // weeks (from configuration)
 
@@ -105,10 +105,55 @@ function StatsComponent() {
     const [selectedPosition, setSelectedPosition] = useState("");
     const [selectedInterval, setSelectedInterval] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
-    const [selectedConference, setSelectedConference] = useState("");
-    const [selectedTeam, setSelectedTeam] = useState("");
     const [selectedFilter, setSelectedFilter] = useState("");
     const [selectedWeek, setSelectedWeek] = useState("");
+    const [activeTeamNameOptions, setActiveTeamNameOptions] = useState([]);
+    const [currentWeek, setCurrentWeek] = useState('');
+
+    useEffect(() => {
+        async function getActiveTeamNameOptions() {
+            setIsLoading(true);
+            try {
+                const activeTeamNameOptionsData = await DatabaseService.getActiveTeams();
+                const sortedTeamNames = activeTeamNameOptionsData.map(team => team.team).sort();
+                setActiveTeamNameOptions(sortedTeamNames);
+            } catch (error) {
+                console.error('An error occurred: ', error);
+                setError('Failed to get active team name options');
+            }
+            setIsLoading(false);
+        }
+
+        getActiveTeamNameOptions();
+    }, []);
+
+    useEffect(() => {
+        async function fetchConfigurations() {
+            try {
+                const fetchedConfigurations = await DatabaseService.getConfiguration();
+                const currentWeekConfig = fetchedConfigurations.find(config => config.configKey === 'current_week');
+                const currentWeek = currentWeekConfig.configValue;
+                setCurrentWeek(currentWeek);
+            } catch (error) {
+                console.error('An error occurred: ', error);
+                setError('Failed to get configurations');
+            }
+        }
+        fetchConfigurations();
+    }, []);
+
+    useEffect(() => {
+        if (selectedCategory === 'conf' || selectedCategory === 'team' || selectedCategory === 'name') {
+            setSelectedFilter('');
+        }
+    }, [selectedCategory]);
+
+    // Might delete this useEffect (usually want to search by the same week)
+    useEffect(() => {
+        if (selectedInterval === 'weekly total' || selectedInterval === 'weekly projected') {
+            setSelectedWeek('');
+        }
+    }, [selectedInterval]);
 
     async function searchPlayerStats(e) {
         e.preventDefault();
@@ -122,8 +167,10 @@ function StatsComponent() {
                 filter: selectedFilter,
                 week: selectedWeek
             });
-            setSearchResults(searchData);
-            console.log(searchData);
+            const filteredSearchData = searchData.filter(player => 
+                player.teamStatus === 'Active'
+            );
+            setSearchResults(filteredSearchData);
         } catch (error) {
             console.error('An error occurred: ', error);
             setError(error);
@@ -147,11 +194,12 @@ function StatsComponent() {
                 <div className="search-container">
                     <form onSubmit={searchPlayerStats}>
                         <div>
-                            <select className="stats-select" 
+                            <select 
+                                className="stats-select" 
                                 value={selectedPosition} 
                                 onChange={(e) => setSelectedPosition(e.target.value)}
                             >
-                                <option value="" disabled>Select a position</option>
+                                <option value="" disabled hidden>Select a position</option>
                                 {positionOptions.map((position) => (
                                     <option key={position} value={position}>{positionDisplayOptions[position]}</option>
                                 ))}
@@ -159,11 +207,13 @@ function StatsComponent() {
                         </div>
                         <div>
 
-                            <select className="stats-select" 
+                            <select 
+                                className="stats-select" 
                                 value={selectedInterval} 
-                                onChange={(e) => setSelectedInterval(e.target.value)}
+                                onChange={(e) => setSelectedInterval(e.target.value)} 
+                                disabled={selectedPosition === ''}
                             >
-                                <option value="" disabled>Select an interval</option>
+                                <option value="" disabled hidden>Select an interval</option>
                                 {intervalOptions.map((interval) => (
                                     <option key={interval} value={interval}>{intervalDisplayOptions[interval]}</option>
                                 ))}
@@ -171,23 +221,28 @@ function StatsComponent() {
                         </div>
                         {(selectedInterval === 'weekly total' || selectedInterval === 'weekly projected') && (
                             <div>
-                                <select className="stats-filter-select"
+                                <select 
+                                    className="stats-filter-select"
                                     value={selectedWeek} 
                                     onChange={(e) => setSelectedWeek(e.target.value)}
                                 >
-                                    <option value="" disabled>Select a week</option>
-                                    {weekOptions.map((week) => (
+                                    <option value="" disabled hidden>Select a week</option>
+                                    {weekOptions
+                                        .filter(week => week <= currentWeek)
+                                        .map((week) => (
                                         <option key={week} value={week}>{weekDisplayOptions[week]}</option>
                                     ))}
                                 </select>
                             </div>
                         )}
                         <div>
-                            <select className="stats-select" 
+                            <select 
+                                className="stats-select" 
                                 value={selectedCategory} 
-                                onChange={(e) => setSelectedCategory(e.target.value)}
+                                onChange={(e) => setSelectedCategory(e.target.value)} 
+                                disabled={selectedPosition === '' || selectedInterval === ''}
                             >
-                                <option value="" disabled>Select a category</option>
+                                <option value="" disabled hidden>Select a category</option>
                                 {categoryOptions.map((category) => (
                                     <option key={category} value={category}>{categoryDisplayOptions[category]}</option>
                                 ))}
@@ -196,20 +251,22 @@ function StatsComponent() {
                         {selectedCategory !== 'all' && selectedCategory !== '' && (
                             <div>
                                 {(selectedCategory === 'conf' || selectedCategory === 'team') ? (
-                                    <select className="stats-filter-select" 
+                                    <select 
+                                        className="stats-filter-select" 
                                         value={selectedFilter}
                                         onChange={(e) => setSelectedFilter(e.target.value)}
                                     >
-                                        <option value="" disabled>Select a filter</option>
+                                        <option value="" disabled hidden>Select a filter</option>
                                         {selectedCategory === 'conf' && conferenceOptions.map((conference) => (
                                             <option key={conference} value={conference}>{conferenceDisplayOptions[conference]}</option>
                                         ))}
-                                        {selectedCategory === 'team' && teamOptions.map((team) => (
-                                            <option key={team} value={team}>{teamDisplayOptions[team]}</option>
+                                        {selectedCategory === 'team' && activeTeamNameOptions.map((team) => (
+                                            <option key={team} value={team}>{teamNameDisplayOptions[team] || team}</option>
                                         ))}
                                     </select>
                                 ) : selectedCategory === 'name' ? (
-                                    <input className="stats-filter-input" 
+                                    <input 
+                                        className="stats-filter-input" 
                                         type="text" 
                                         value={selectedFilter} 
                                         placeholder="Enter Name" 
@@ -219,7 +276,19 @@ function StatsComponent() {
                             </div>
                         )}
                         <div>
-                            <button className="stats-button" type="submit" disabled={isLoading}>Search</button>
+                            <button 
+                                className="stats-button" 
+                                type="submit" 
+                                disabled=
+                                    {isLoading || 
+                                    (selectedPosition === '' && selectedInterval === '' && selectedCategory === '') || 
+                                    (selectedInterval === 'weekly total' && selectedWeek === '') ||
+                                    (selectedInterval === 'weekly projected' && selectedWeek === '') ||
+                                    (selectedCategory !== 'all' && selectedCategory !== '' && selectedFilter === '')
+                                }
+                            >
+                                Search
+                            </button>
                             <button className="stats-button" onClick={clearSearch} disabled={isLoading}>Clear</button>
                         </div>
                     </form>
@@ -228,6 +297,13 @@ function StatsComponent() {
                             <div className="table-container">
                                 <table>
                                     <thead>
+                                        <tr>
+                                            <th colSpan="6">Description</th>
+                                            <th colSpan="7">Passing</th>
+                                            <th colSpan="3">Rushing</th>
+                                            <th colSpan="2">Extra</th>
+                                            <th colSpan="2">Points</th>
+                                        </tr>
                                         <tr>
                                             <th>Conf</th>
                                             <th>Team</th>
